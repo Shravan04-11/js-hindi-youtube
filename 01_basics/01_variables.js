84df86925b7bbf17604ffcc859197839b48485b2
Shravan04-11 <ashravan338@gmail.com>
2025-06-28 16:17:24 +0000

    commitechnge

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 144553
+let accountEmail = "bc@b.com"
+var accountPassword = 12345
+accountCity = 'New York';
+
+console.table(['accountId','accountEmail','accountPassword','accountCity'])
