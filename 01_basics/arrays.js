09cd4d8df9622e5277bec2ea46bf01fe1ddbcc3b
Shravan04-11 <ashravan338@gmail.com>
2025-07-01 16:09:14 +0000

    commit

---
+++ 01_basics/arrays.js
@@ -0,0 +1,18 @@
+const myArr = [0,1,2,3,4,5]
+
+const myHeros = ["spiderman", "batman", "superman", "ironman"]
+
+const myArr2 = new Array(1,2,3,4,5)
+
+console.log(myArr[1]);
+
+//Array methods
+
+myArr.push(6) // adds to the end of the array
+myArr.push(7)
+myArr.pop() // removes the last element of the array
+
+myArr.unshift(0)
+
+myArr.shift() // removes the first element of the array
+console.log(myArr);
