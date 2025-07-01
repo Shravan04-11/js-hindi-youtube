4101e7c3907b6439118e1cb60fa3bf503baafea2
Shravan04-11 <ashravan338@gmail.com>
2025-07-01 16:53:30 +0000

    commit

---
+++ 01_basics/objects.js
@@ -0,0 +1,14 @@
+// singleton
+
+// object literals
+
+const JsUser = {
+  name: "Shravan",
+  age:21,
+  location:'Andhra',
+  email: 'shravan.411100@gmail.com',
+  isLoggedIn: false,
+  lastLoginDays: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+
+}
+console.log(JsUser.email);
