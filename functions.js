b5a3b727c93c60e54251f92e10439e5daef5b3b5
Shravan04-11 <ashravan338@gmail.com>
2025-07-02 16:18:19 +0000

    commit

---
+++ functions.js
@@ -0,0 +1,17 @@
+function sayMyName(){
+  console.log("S")
+  console.log("H")
+  console.log("R")
+  console.log("A")
+  console.log("V")
+  console.log("A")
+  console.log("N")
+}
+
+sayMyName()
+
+function addTwoNumbers(num1, num2){
+  console.log(num1 + num2)
+}
+
+addTwoNumbers(2, 3)
